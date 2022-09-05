31286ecc90acb1837934915ce013d79dc13b03ea
JannikE03 <jannik@5eschler.com>
2022-09-05 20:07:03 +0200

    Edited the C_Particle class further and added a C_Box class

---
+++ Project/Part 1/Python Code/C_Box.py
@@ -0,0 +1,16 @@
+import numpy as np
+import random
+from scipy.constants import Boltzmann as k
+
+
+class Box:
+    def __init__(self, L, nozzle_area, nozzle_pos="x+"):
+        self.length = L  # Length of one side of the box
+        self.nozzle_area = nozzle_area  # Nozzle Area
+        self.nozzle_rad = np.sqrt(nozzle_area/np.pi)  # Nozzle Radius
+        self.nozzle_pos = nozzle_pos  # Position where the nozzle is located on
+        pos_dict = {"x+": [1, 1], "x-": [1, -1], "y+": [2, 1], "y-": [2, -1], "z+": [3, 1], "z-": [3, -1]}  # Just a dictionary to define the nozzle_axis and nozzle_side parameters from the nozzle_pos input
+        self.nozzle_axis = pos_dict[nozzle_pos][0]  # Axis which the nozzle is positioned on. x = 0, y = 1, z = 3
+        self.nozzle_side = pos_dict[nozzle_pos][1]  # Side which the nozzle is on (-1 for negative, 1 for positive)
+
+
--- Project/Part 1/Python Code/C_Particle.py
+++ Project/Part 1/Python Code/C_Particle.py
@@ -11,7 +11,7 @@ from scipy.constants import Boltzmann as k
 class Particle:
     particle_num = 0
 
-    def __init__(self, m, T, L, seed, timesteps=1e-13):
+    def __init__(self, m, T, L, seed, box, timesteps=1e-13):
         self.name = f"Particle_{Particle.particle_num}"
         Particle.particle_num += 1
 
@@ -20,28 +20,43 @@ class Particle:
         self.v_sigma = np.sqrt(m / k * T)
         self.v_mu = 0
         random.seed(a=seed, version=2)
-        self.L = L
+        self.box = box
 
-        self.position = np.array([random.uniform(-L/2, L/2) for _ in range(3)])
+
+
+        self.position = np.array([random.uniform(-self.box.length/2, self.box.length/2) for _ in range(3)])
         self.velocity = np.array([random.gauss(self.v_mu, self.v_sigma) for _ in range(3)])
 
     def advance(self):
-        if self.position[-1][0] <= -self.L/2 or self.L/2 <= self.position[-1][0]:
-            self.wall_collision("x")
-        elif self.position[-1][1] <= -self.L/2 or self.L/2 <= self.position[-1][1]:
-            self.wall_collision("y")
-        elif self.position[-1][2] <= -self.L/2 or self.L/2 <= self.position[-1][2]:
-            self.wall_collision("z")
-        else:
-            np.append(self.position, self.position[-1] + self.velocity * self.dt)
+        for dimension in range(3):
+            if abs(self.position[-1][dimension]) >= self.box.length/2:
+                self.wall_collision(dimension)
+            else:
+                np.append(self.velocity, self.velocity)
+                np.append(self.position, self.position[-1] + self.velocity * self.dt)
 
-    def wall_collision(self, direction):
-        if direction == "x":
-            np.append(self.velocity, [-self.velocity[0], self.velocity[1], self.velocity[2]])
-            np.append(self.position, [self.L - self.position[-1], self.velocity * self.dt, self.velocity * self.dt])
-        elif direction == "y":
-            np.append(self.velocity, [self.velocity[0], -self.velocity[1], self.velocity[2]])
-        elif direction == "z":
-            np.append(self.velocity, [self.velocity[0], self.velocity[1], -self.velocity[2]])
+    def wall_collision(self, dimension):
+        side = np.sign(self.velocity[-1][dimension])  # Checking which side of the box we are on (positive or negative)
+
+        np.append(self.velocity, self.velocity)  # Appending same velocity
+        np.append(self.position, self.position[-1] + self.velocity * self.dt)  # Continuing movement in same direction
+
+        if self.exiting_nozzle():
+            self.inside_box = False
+        else:
+            self.velocity[-1][dimension] = -self.velocity[-1][dimension]  # Changing direction of velocity in correct dimension
+            self.position[-1][dimension] = side * self.box.length - self.position[-2][dimension]  # Changing position to position after collision
+
+    def exiting_nozzle(self):
+        in_x_rad1 = (np.sqrt((self.position[-1][1])**2 + (self.position[-1][2])**2) < self.box.nozzle_rad)  # Checking if the particle is inside the radius of the nozzle before exiting
+        in_x_rad2 = (np.sqrt((self.position[-2][1]) ** 2 + (self.position[-2][2]) ** 2) < self.box.nozzle_rad)  # Checking if the particle is inside the radius of the nozzle after exiting
+        moving_to_nozzle = (self.velocity[-1][0] > 0)  # Checking if the particle is moving towards the nozzle in x
+        if in_x_rad1 and in_x_rad2 and moving_to_nozzle:
+            return True
+        else:
+            return False
+
+
+
 
 
