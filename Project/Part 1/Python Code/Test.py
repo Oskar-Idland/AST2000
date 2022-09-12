88eece8930d0ea0628c337322e564f6d4b3bfcda
JannikE03 <jannik@5eschler.com>
2022-09-12 07:48:42 +0200

    Small fixes on C_Particle.py

--- Project/Part 1/Python Code/Test.py
+++ Project/Part 1/Python Code/Test.py
@@ -1,4 +1,19 @@
 import numpy as np
-L = 1
-x = np.array([[0, 1, 2], [2, 1, 0]])
-print(np.nonzero(x >= 1))
+
+def advance_out(instance):
+    instance.c += 1
+
+
+class Test:
+    c = 0
+    def __init__(self, c):
+        self.c = c
+
+    @classmethod
+    def advance(self):
+        self.c += 1
+
+x = np.array([Test(np.array([0, 2, 1, 5])), Test(np.array([5, 1, 8, 3]))])
+map(Test.advance, x)
+x.advance()
+print(x[0].c)
