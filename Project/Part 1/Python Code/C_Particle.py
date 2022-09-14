64cb76232317a54eb99d48383f6e32c9291f267d
Oskar-Idland <oskaridland@hotmail.com>
2022-09-14 14:33:38 +0200

    Simulation time shortened by a factor of 5

--- Project/Part 1/Python Code/C_Particle.py
+++ Project/Part 1/Python Code/C_Particle.py
@@ -5,7 +5,6 @@ from scipy.constants import Boltzmann as k
 import matplotlib.pyplot as plt
 # FUNCTIONS -------------------
 from C_Box import Box
-from numba import njit
 # CLASSES ---------------------
 
 class Particle:
@@ -25,29 +24,29 @@ class Particle:
 
     def rand_pos_vel(self):
         random.seed(a=self.seed, version=2)
-        self.position = np.array([[random.uniform(-self.box.length / 2, self.box.length / 2) for _ in range(3)]])
-        self.velocity = np.array([[random.gauss(self.v_mu, self.v_sigma) for _ in range(3)]]) #Added a factor of 10^4 for velocity to make sense
+        self.position = np.array([random.uniform(-self.box.length / 2, self.box.length / 2) for _ in range(3)])
+        self.velocity = np.array([random.gauss(self.v_mu, self.v_sigma) for _ in range(3)]) #Added a factor of 10^4 for velocity to make sense
     
     def advance(self, timestep=1e-12):
-        self.velocity = np.append(self.velocity, np.array([self.velocity[-1]]), axis = 0)  # Appending same velocity
-        self.position = np.append(self.position, np.array([self.position[-1] + self.velocity[-1] * timestep]), axis = 0)  # Continuing movement in same direction
+        # Appending same velocity
+        self.position = self.position + self.velocity * timestep # Continuing movement in same direction
 
-        collision_axis = np.nonzero(abs(self.position[-2]) >= self.box.length/2)  # Checking for each dimension whether the position is outside the length of the box
+        collision_axis = np.nonzero(abs(self.position) >= self.box.length/2)  # Checking for each dimension whether the position is outside the length of the box
         if collision_axis[0].size > 0:
             self.wall_collision(collision_axis[0][0])  # Initiating a collision with a wall
 
     def wall_collision(self, axis):
-        side = np.sign(self.position[-1][axis])  # Checking which side of the box we are on (positive or negative)
+        side = np.sign(self.position[axis])  # Checking which side of the box we are on (positive or negative)
 
         if self.exiting_nozzle(axis):  # Checking if the particle is going through the nozzle
-            self.v_exit += abs(self.velocity[-1][2])  # Storing the speed of the particle when exiting in the z-direction
+            self.v_exit += abs(self.velocity[2])  # Storing the speed of the particle when exiting in the z-direction
 
-        self.velocity[-1][axis] = -self.velocity[-1][axis]  # Changing direction of velocity in correct dimension
-        self.position[-1][axis] = side * self.box.length - self.position[-2][axis]  # Changing position to position after collision
+        self.velocity[axis] = -self.velocity[axis]  # Changing direction of velocity in correct dimension
+        self.position[axis] = side * self.box.length - self.position[axis]  # Changing position to position after collision
 
     def exiting_nozzle(self, axis):
-        dist_from_nozzle = self.position[-2] - self.box.nozzle_pos
-        in_nozzle_rad = np.linalg.norm(dist_from_nozzle[:2]) < self.box.nozzle_rad
+        dist_from_nozzle = self.position - self.box.nozzle_pos
+        in_nozzle_rad = np.linalg.norm(dist_from_nozzle) < self.box.nozzle_rad
         # in_nozzle_rad = (np.sqrt(np.linalg.norm(self.position[-2])**2 - (self.position[-2][axis])**2) < self.box.nozzle_rad)  # Checking if the particle is inside the radius of the nozzle before exiting
         # moving_to_nozzle = (np.sign(self.velocity[-1][axis]) == np.sign(self.box.nozzle_pos[axis]))  # Checking if the particle is moving towards the nozzle in x
         if in_nozzle_rad:  # Checking if the particle is moving towards, and hitting the correct wall as well as being inside the nozzle diameter before and after exiting the nozzle
--- Project/Part 1/Python Code/Engine_Sim.py
+++ Project/Part 1/Python Code/Engine_Sim.py
@@ -4,7 +4,7 @@ from scipy.constants import Avogadro as A
 from molmass import Formula
 from C_Box import Box
 import time
-
+start = time.time()
 
 """
 Simplifications and Assumptions:
@@ -19,7 +19,7 @@ Simplifications and Assumptions:
 seed = 8
 m = Formula('H2').mass/(A*500) # Mass of one H2 molecule In Kg
 L = 1E-6 
-T = 3*1E3
+T = 3*1E4
 N = int(1e3)
 timesteps = 1000
 nozzle_pos = np.array([0,0,-L/2]) # Nozzle positioned directly under the box 
@@ -32,12 +32,14 @@ particles = np.array([Particle(m,T,seed*i,Box) for i in range(N)])
 b = time.time()
 print(f'Particle creation took {b - a} s')
 # Advancing all particles 1000 times 
-start = time.time()
+a = time.time()
 [[list(map(lambda p: p.advance(), particles))] for _ in range(timesteps)]
-end = time.time()
-print(f'Advancing particles took {end - start} s')
+b = time.time()
+print(f'Advancing particles took {b - a} s')
 
 a = time.time()
 print('Momentum: ' + str(10*error_factor*m*np.sum(np.array(list(map(lambda p: p.v_exit, particles)))))) # Multiplying by error factor to correct for the momentum being counted twice as much as it should
 b = time.time()
 print(f'Momentum calculation took {b - a} s')
+end = time.time()
+print(f'The program used {end - start} s')
