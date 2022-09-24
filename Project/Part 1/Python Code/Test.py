d4433af7995923f2fe978441c210f7ba5bcecc22
Oskar-Idland <oskaridland@hotmail.com>
2022-09-24 21:38:08 +0200

    Part_C Results done

--- Project/Part 1/Python Code/Test.py
+++ Project/Part 1/Python Code/Test.py
@@ -1,68 +1,15 @@
 import numpy as np
-from C_Particle import Particle
-from C_Box import Box
-import matplotlib.pyplot as plt
-from scipy.constants import Avogadro as A
-from molmass import Formula
-from itertools import product, combinations
+import ast2000tools.utils as utils
+from ast2000tools.solar_system import SolarSystem
+from ast2000tools.space_mission import SpaceMission
 
-# x = np.array([Particle(1,3000,2,Box(1e-6,np.array([0,0,-1e-6/2]))),Particle(1,3000,4,Box(1e-6,np.array([0,0,-1e-6/2])))])
-
-# x = np.array([[1,2,3]])
-# print(x)
-# x = np.append(x,np.array([[4,5,6]]), axis=0)
-# print(x[-1])
-
-seed = 0
-m = Formula('H2').mass/(A*1000) # Mass of one H2 molecule In Kg
-L = 1E-6 
-T = 3*1E3
-box = Box(L, np.array([0,0,-L/2]))
-p = Particle(m, T, seed, box)
-
-[p.advance() for _ in range(1000)]
-
-
-
-# box_cordx = [-L/2, L/2, L/2, -L/2, -L/2]
-# box_cordy = [-L/2, -L/2, L/2, L/2, -L/2]
-# nozzle_cordx = [-box.nozzle_rad, box.nozzle_rad]
-# nozzle_cordy = [-L/2, -L/2]
-# plt.plot(box_cordx,box_cordy, c = 'blue')
-# plt.hlines([-L/2, -L/2], -box.nozzle_rad, box.nozzle_rad, color = 'orange', label = 'nozzle')
-# print(p.position)
-px = []
-for arr in p.position:
-    px.append(arr[0])
-
-py = []
-for arr in p.position:
-    py.append(arr[1])
-
-pz = []
-for arr in p.position:
-    pz.append(arr[2])
-
-
-print('--------------------------------')
-print('Momentum: ' + str(m*p.v_exit))
-
-
-fig = plt.figure()
-ax = plt.axes(projection = '3d')
-
-# Draw the cube
-r = [-L/2, L/2]
-for s, e in combinations(np.array(list(product(r, r, r))), 2):
-    if np.sum(np.abs(s-e)) == r[1]-r[0]:
-        ax.plot3D(*zip(s, e), color="blue", alpha=0.4)
-
-ax.plot3D(px,py,pz, c = 'red', label = 'path')
-ax.scatter(px[0],py[0], pz[0], label = 'start')
-ax.scatter(px[10],py[10], pz[10], label = 'first 10 steps')
-ax.scatter(px[-1],py[-1], pz[-1], label = 'end')
-# Nozzle
-x = np.linspace(0,2*np.pi, 100)
-ax.plot(box.nozzle_rad*np.cos(x), box.nozzle_rad*np.sin(x), -L/2, label = 'Nozzle', c = 'black')
-ax.legend()
-plt.show()
+username = "janniesc"
+seed = utils.get_seed(username)
+system = SolarSystem(seed)
+mission = SpaceMission(seed)
+star_mass = system.star_mass
+for i in range(len(system.initial_positions[0])):
+    mass = system.masses[i]
+    r = np.array([system.initial_positions[0][i],system.initial_positions[1][i]])
+    print(abs(mass/np.linalg.norm(r)**2))
+print(r)
--- Project/Part 2/Python Code/Part_C.py
+++ Project/Part 2/Python Code/Part_C.py
@@ -1,10 +1,9 @@
 import matplotlib.pyplot as plt
-from numba import jit
+from numba import njit
 import numpy as np
 import ast2000tools.utils as utils
 from ast2000tools.solar_system import SolarSystem
 from ast2000tools.space_mission import SpaceMission
-print('Imports done')
 
 #CONSTANTS
 G = 4*(np.pi**2)  #Gravitational Constant
@@ -19,18 +18,19 @@ T = 13*20  #You want to find the planetary position from t=0 to t=T. How could y
 N = 30000  #Number of time steps
 dt = T/N  #calculate time step from T and N
 
-@jit(nopython = True)                       #Optional, but recommended for speed, check the numerical compendium
+                       #Optional, but recommended for speed, check the numerical compendium
+# Functions
+@njit
 def integrate(T, dt, N, S_x0, S_y0, S_vx0, S_vy0, P_x0, P_y0, P_vx0, P_vy0, G, sun_mass, planet_mass):
-    print('Integration started')
     t = np.linspace(0.0, T, N)
-    x_Star = np.zeros((N, 2))
-    x_Planet = np.zeros((N, 2))
+    r_Star = np.zeros((N, 2))
+    r_Planet = np.zeros((N, 2))
     v_Star = np.zeros((N, 2))
     v_Planet = np.zeros((N, 2))
 
-    x_Planet[0] = [P_x0, P_y0]
+    r_Planet[0] = [P_x0, P_y0]
     v_Planet[0] = [P_vx0, P_vy0]
-    x_Star[0] = [S_x0, S_y0]
+    r_Star[0] = [S_x0, S_y0]
     v_Star[0] = [S_vx0, S_vy0]
 
     for i in range(N-1):
@@ -42,29 +42,53 @@ def integrate(T, dt, N, S_x0, S_y0, S_vx0, S_vy0, P_x0, P_y0, P_vx0, P_vy0, G, s
         disadvantages; refer to the Numerical Compendium to learn about these.
         '''
         # Using Leapfrog method
-        r_norm_Planet = np.linalg.norm(x_Planet[i])
-        r_norm_Star = np.linalg.norm(x_Star[i])
+        r = r_Planet[i] - r_Star[i]
+        r_norm = np.linalg.norm(r)
+        # r_norm_Planet = np.linalg.norm(r_Planet[i])
+        # r_norm_Star = np.linalg.norm(r_Star[i])
 
-        a_Planet = -G * sun_mass * x_Planet[i] / (r_norm_Planet ** 3)
-        a_Star = -G * planet_mass * x_Star[i] / (r_norm_Star ** 3)
+        a_Planet = -G * sun_mass * r / (r_norm ** 3)
+        a_Star = G * planet_mass * r / (r_norm ** 3)
 
-        vh_Planet = v_Planet[i] + a_Planet[i]*dt/2
-        vh_Star = v_Star[i] + a_Star[i]*dt/2
+        vh_Planet = v_Planet[i] + a_Planet*dt/2
+        vh_Star = v_Star[i] + a_Star*dt/2
 
-        x_Planet[i+1] = x_Planet[i] + vh_Planet*dt
-        x_Star[i+1] = x_Star[i] + vh_Star*dt
+        r_Planet[i+1] = r_Planet[i] + vh_Planet*dt
+        r_Star[i+1] = r_Star[i] + vh_Star*dt
 
-        r_norm_Planet = np.linalg.norm(x_Planet[i+1])
-        r_norm_Star = np.linalg.norm(x_Star[i+1])
+        r =   r_Planet[i+1] - r_Star[i+1]
+        r_norm = np.linalg.norm(r)
 
-        a_Planet = -G * sun_mass * x_Planet[i+1] / (r_norm_Planet ** 3)
-        a_Star = -G * planet_mass * x_Star[i+1] / (r_norm_Star ** 3)
+        # r_norm_Planet = np.linalg.norm(r_Planet[i+1])
+        # r_norm_Star = np.linalg.norm(r_Star[i+1])
+
+        a_Planet = -G * sun_mass * r / (r_norm ** 3)
+        a_Star = G * planet_mass * r / (r_norm ** 3)
 
         v_Planet[i+1] = vh_Planet + a_Planet*dt/2
         v_Star[i+1] = vh_Star + a_Star*dt/2
-    return t, x_Planet, v_Planet, x_Star, v_Star
 
-print('Defining integration function complete')
+    return t, r_Planet, v_Planet, r_Star, v_Star
+
+def E(mu,v,r,M):
+    '''
+    Calculates the energy in the system \n
+    mu is the reduced mass \n
+    v = Planet_vel - Star_vel \n
+    r = Planet_pos - Star_pos \n
+    M = Planet_mass + Star_mass
+    '''
+    return np.linalg.norm(1/2*mu*v**2 - (G*M*mu)/np.linalg.norm(r))
+
+def P(r,v,mu):
+    '''
+    Calculates the angular momentum of the system \n
+    r = Planet_pos - Star_pos \n
+    v = Planet_vel - Star_vel \n
+    mu is the reduced mass
+    '''
+    return np.cross(r,mu*v)
+
 # Initializing system
 username = "janniesc"
 seed = utils.get_seed(username)
@@ -73,12 +97,13 @@ mission = SpaceMission(seed)
 star_mass = system.star_mass
 planet_mass = system.masses[0] 
 
-P_x0 = system.initial_positions[0,0]
-P_y0 = system.initial_positions[1,0]
-P_vx0 = system.initial_velocities[0,0] 
-P_vy0 = system.initial_velocities[1,0]
-S_vx0 = (P_vx0*planet_mass) / star_mass
-S_vy0 = (P_vy0*planet_mass) / star_mass
+Planet = 2
+P_x0 = system.initial_positions[0,Planet]
+P_y0 = system.initial_positions[1,Planet]
+P_vx0 = system.initial_velocities[0,Planet] 
+P_vy0 = system.initial_velocities[1,Planet]
+S_vx0 = -(P_vx0*planet_mass) / star_mass
+S_vy0 = -(P_vy0*planet_mass) / star_mass
 
 com = 1/(star_mass + planet_mass) * (planet_mass*np.array([P_x0,P_y0]))  # Calculating the center of mass assuming star is in origin
 
@@ -88,12 +113,74 @@ P_y0 -= com[1]
 S_x0 = -com[0]
 S_y0 = -com[1]
 
-print('System initialized and initial values defined')
-t, x_Planet, v_Planet, x_Star, v_Star  = integrate(T, dt, N, S_x0, S_y0, S_vx0, S_vy0, P_x0, P_y0, P_vx0, P_vy0, G, star_mass, planet_mass)
-print('Integration complete')
 
-plt.plot(x_Planet[:,0], x_Planet[:,1], label = 'Planet Orbit')
-plt.plot(x_Star[:,0], x_Star[:,1], label = 'Star Orbit')
+t, r_Planet, v_Planet, r_Star, v_Star  = integrate(T, dt, N, S_x0, S_y0, S_vx0, S_vy0, P_x0, P_y0, P_vx0, P_vy0, G, star_mass, planet_mass)
+
+
+r = (P_x0**2 + P_y0**2)**0.5
+t = np.linspace(0, 2*np.pi, 1000)
+x = r*np.cos(t)
+y = r*np.sin(t)
+plt.plot(x,y, label = 'Perfectly round planet orbit ')
+r = (S_x0**2 + S_y0**2)**0.5
+x = r*np.cos(t)
+y = r*np.sin(t)
+plt.plot(x,y, label = 'Perfectly round star orbit')
+
+plt.plot(r_Planet[:,0], r_Planet[:,1], label = 'Planet Orbit', linestyle = '--')
+plt.plot(r_Star[:,0], r_Star[:,1], label = 'Star Orbit', linestyle = '--')
 plt.scatter([0],[0], label = 'Center of mass')
+plt.scatter(r_Planet[0][0], r_Planet[0][1], label = 'Planet start position')
+plt.scatter(r_Planet[-1][0],r_Planet[-1][1], label = 'Planet end position' )
+plt.xlabel("x-position [AU]")
+plt.ylabel("y-position [AU]")
 plt.legend()
+plt.axis('equal')
+
 plt.show()
+
+M = planet_mass + star_mass
+mu = (planet_mass * star_mass)/M
+v = v_Planet - v_Star
+r = r_Planet - r_Star 
+resolution = 1000
+t = [i for i in range(int(resolution))]
+energy = []
+for i in range(N):
+    if i % (N/resolution) == 0:
+        energy.append(E(mu, v[i], r[i], M))
+
+angular_momentum = []
+for i in range(N):
+    if i % (N/resolution) == 0:
+        angular_momentum.append(P(r[i], v[i],mu))
+        
+print(max(energy)/min(energy), max(energy), min(energy))
+        
+# fig, ax1 = plt.subplots()
+
+# P_plot = ax1.plot(t,angular_momentum, label="Angular Momentum", c = 'blue')
+# ax1.tick_params(axis='y')
+# ax1.set_ylabel('Angular Momentum')
+
+# ax2 = ax1.twinx()
+# E_plot = ax2.plot(t,energy, label = 'Energy', c = 'red')
+# ax2.tick_params(axis='y')
+# ax2.set_ylabel('Energy')
+
+# plt.grid()
+# plt.tight_layout()
+# lns = P_plot + E_plot
+# labels = [l.get_label() for l in lns]
+# plt.legend(lns, labels)
+# plt.show()
+
+
+
+# for i in range(N):
+#     if i % (N/10) == 0: #Ensures only ten elements are calculated
+#         print(f'Energy in the system at {int(i/N*10 + 1)}/10: {E(mu, v[i], r[i], M)}')
+
+# for i in range(N):
+#     if i % (N/10) == 0: #Ensures only ten elements are calculated
+#         print(f'Angular momentum in the system at {int(i/N*10 + 1)}/10: {P(r[i], v[i], mu)}')
