f4bfb4697fda5abddb90db6dc3e8c19ded01324c
Oskar-Idland <oskaridland@hotmail.com>
2022-11-04 09:57:44 +0100

    fixed all bugs in exercise B and is complete

--- Project/Part 5/Python Files/Part_5_A.py
+++ Project/Part 5/Python Files/Part_5_A.py
@@ -1,8 +1,9 @@
 import numpy as np
-import scipy as sp  
+import scipy as sp
 import os
 import matplotlib.pyplot as plt
 import ast2000tools.utils as utils
+from numba import njit
 from ast2000tools.solar_system import SolarSystem
 from ast2000tools.space_mission import SpaceMission
 G = 4*np.pi**2
@@ -14,79 +15,60 @@ system = SolarSystem(seed)
 mission = SpaceMission(seed)
 star_mass = system.star_mass
 sum_planet_mass = np.sum(system.masses)
+initial_positions = system.initial_positions
+masses = system.masses
 
+@njit
 def trajectory(t: float, T: float, dt: float, v0: np.ndarray, r0: np.ndarray):
     '''
     Finds the trajectory of the space shuttle \n
     Returns the final time (yr), velocity(AU) and position(AU)
     '''
     N = int((T-t)/dt)
-    r = np.zeros((N,2)); r[0] = r0
-    v = np.zeros((N,2)); v[0] = v0
-    
+    r = np.zeros((N, 2))
+    r[0] = r0
+    v = np.zeros((N, 2))
+    v[0] = v0
 
     '''
     We simplify the system by adding all positions, velocities and masses of the planets and star
     '''
-    m_system = star_mass + sum_planet_mass 
-    r_system = np.array([0.0,0.0]); v_system = np.array([0.0,0.0]);
-    com = np.array([0.0,0.0]); tot_momentum = 0
-    for body in range(system.number_of_planets):  # Finds 
-        planet_mass = system.masses[body]
-        planet_position = system.initial_positions[:,body]
-        com += planet_mass * planet_position
-        
-        planet_velocity = system.initial_velocities[:,body]
-        momentum = planet_mass*planet_velocity
-        tot_momentum += momentum
-    com *= 1/m_system
-    r_system = com
-    v_system = tot_momentum / m_system
-    # Integration loop
-    # As the shuttle mass is so small in comparison to the mass of the system, we disregard the gravitational force from the shuttle on the system.  
+
     for i in range(N-1):
-        # Calculating the acceleration as a N-body system
-        r_shuttle_system = r[i] - r_system
-        a = -G *(m_system)/(np.linalg.norm(r_shuttle_system)**3) * r_shuttle_system
-        
         # Calculating the acceleration of each body individually
-        # a = -G *(star_mass)/(np.linalg.norm(r[i])) * r[i] # Acceleration from star
-        # for p in range(8):
-        #     r_shuttle_p = r[i] - system.initial_positions[:,p]
-        #     a += -G *(system.masses[p])/(np.linalg.norm(r_shuttle_p)) * r_shuttle_p  
-            
+        a = -G *(star_mass)/(np.linalg.norm(r[i])**3) * r[i] # Acceleration from star
+        for p in range(8):
+            r_shuttle_p = r[i] - initial_positions[:,p]
+            a += -G *(masses[p])/(np.linalg.norm(r_shuttle_p)**3) * r_shuttle_p
+
         vh = v[i] + a * dt / 2
-        r[i+1] = r[i] +  vh * dt
-        r_system = r_system + v_system*dt # To be commented out when calculating each body individually. 
-        
-        r_shuttle_system = r[i+1] - r_system # To be commented out when calculating each body individually.
-        
+        r[i+1] = r[i] + vh * dt
+
         # Calculating the acceleration of each body individually
-        # a = -G *(star_mass)/(np.linalg.norm(r[i])) * r[i] # Acceleration from star
-        # for p in range(8):
-        #     r_shuttle_p = r[i] - system.initial_positions[:,p]
-        #     a += -G *(system.masses[p])/(np.linalg.norm(r_shuttle_p)) * r_shuttle_p
-        
-        # Calculating the acceleration as a N-body system
-        a = -G *(m_system)/(np.linalg.norm(r_shuttle_system)**3) * r_shuttle_system
+        a = -G *(star_mass)/(np.linalg.norm(r[i])**3) * r[i] # Acceleration from star
+        for p in range(8):
+            r_shuttle_p = r[i] - initial_positions[:,p]
+            a += -G *(masses[p])/(np.linalg.norm(r_shuttle_p)**3) * r_shuttle_p
+
         v[i+1] = vh + a * dt / 2
-        
+
     return T, v, r
 
 
 if __name__ == "__main__":
     planet_file = np.load('planet_trajectories.npz')
-    dt = planet_file['times'][1] # Nice to have the same time index as used by SpaceMission
+    # Nice to have the same time index as used by SpaceMission
+    dt = planet_file['times'][1]
     planet_r = utils.km_to_AU(system.radii[0])
-    v0 = np.array([1,1])
-    r0 = np.array([planet_r + system.initial_positions[0,0], 0])
+    v0 = np.array([-4, 4])
+    r0 = np.array([planet_r + system.initial_positions[0, 0], 0])
     t, v, r = trajectory(0, 2, dt, v0, r0)
-    plt.scatter(r[0,0], r[0,1], label = 'Beginning')
-    plt.plot(r[:,0],r[:,1], label="Shuttle trajectory")
+    plt.scatter(r[0, 0], r[0, 1], label='Beginning')
+    plt.plot(r[:, 0], r[:, 1], label="Shuttle trajectory")
     plt.xlabel("x [AU]")
     plt.ylabel("y [AU]")
     plt.axis('equal')
     plt.legend()
-    print(system.initial_positions[:,0])
-    print(system.initial_positions[:,1])
+    print(system.initial_positions[:, 0])
+    print(system.initial_positions[:, 1])
     plt.show()
--- Project/Part 5/Python Files/Part_5_B.py
+++ Project/Part 5/Python Files/Part_5_B.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scipy as sp  
+import scipy as sp
 import os
 import matplotlib.pyplot as plt
 import ast2000tools.utils as utils
@@ -12,148 +12,150 @@ username = "janniesc"
 seed = utils.get_seed(username)
 system = SolarSystem(seed)
 
+
 @njit
-def find_closest_distance(planet_trajectory1, planet_trajectory2):
+def find_closest_orbit(planet_trajectory1, planet_trajectory2):
     '''
     Finds the time and distance where to bodies are the closest \n
     Main use is to find the best time to launch the shuttle \n
     Returns smallest distance and time index
     '''
-    smallest_distance = np.linalg.norm(planet_trajectory1[:,0] - planet_trajectory2[:,0])
+    smallest_distance = np.linalg.norm(
+        planet_trajectory1[:, 0] - planet_trajectory2[:, 0])
     time_index = 0
     for t in range(len(time)):
-        distance = np.linalg.norm(planet_trajectory1[:,t] - planet_trajectory2[:,t])
+        distance = np.linalg.norm(
+            planet_trajectory1[:, t] - planet_trajectory2[:, t])
         if distance < smallest_distance:
             smallest_distance = distance
-            time_index = t    
-            
+            time_index = t
+
     return smallest_distance, time_index
+
+
+@njit
+def min_distance_shuttle_target():
+    min_distance = 10000
+    index = 0
+    for t in range(time_index, final_time_index):
+        distance = np.linalg.norm(
+            target_planet_trajectory[:, t] - shuttle_position[int(t - time_index), :])
+        if distance < min_distance:
+            min_distance = distance
+            index = t
+
+    return index, final_time_index, min_distance
+
 
 def check_close_enough():
     '''
     Just for a quick check of how close the shuttle is to the target
     '''
-    min_distance = 10000; index = 0
+
+    min_distance = 10000
+    index = 0
     for t in range(time_index, final_time_index):
-        distance = np.linalg.norm(target_planet_trajectory[:,t] - shuttle_position[int(t - time_index),:])
+        distance = np.linalg.norm(
+            target_planet_trajectory[:, t] - shuttle_position[int(t - time_index), :])
         if distance < min_distance:
             min_distance = distance
             index = t
-    
+
+    print(index)
+
+    # Rask versjon av løkken over, men returnerer feil index
+    # index, final_time_index, min_distance = min_distance_shuttle_target()
+
+    plt.scatter(shuttle_position[int(index - time_index), 0], shuttle_position[int(
+        index - time_index), 1])
+
+    plt.scatter(target_planet_trajectory[0, index],
+                target_planet_trajectory[1, index])
 
     planet_m = system.masses[1]
     star_m = system.star_mass
-    distance_to_star = np.linalg.norm(shuttle_position[int(index - time_index)])
+    distance_to_star = np.linalg.norm(
+        shuttle_position[int(index - time_index)])
     l = distance_to_star*np.sqrt(planet_m/10*star_m)
-    
+    print(f'\t \t Min distance: {min_distance: .2e}')
+
     if min_distance < l:
-        print(f'Min distance: {min_distance}')
         print('Close enough!!')
-        plt.scatter(shuttle_position[int(index - time_index), 0], shuttle_position[int(index - time_index),1], label = 'Shuttle position close enough')
-        plt.scatter(target_planet_trajectory[0,index], target_planet_trajectory[1,index], label = 'Target planet position close enough')
-        print(f'Total time of travel is {(final_time_index-index)*dt: .2f} years')  
-        return True    
-        
+        plt.scatter(shuttle_position[int(index - time_index), 0], shuttle_position[int(
+            index - time_index), 1], label='Shuttle position close enough')
+        plt.scatter(target_planet_trajectory[0, index], target_planet_trajectory[1,
+                    index], label='Target planet position close enough')
+        print(
+            f'Total time of travel is {(final_time_index-index)*dt: .2f} years')
+        return True
 
 
 if __name__ == "__main__":
     planet_file = np.load('planet_trajectories.npz')
     planet_positions = planet_file['planet_positions']
     time = planet_file['times']
-    home_planet_trajectory = planet_positions[:,0,:]
-    target_planet_trajectory = planet_positions[:,1,:]
-    smallest_distance, time_index = find_closest_distance(home_planet_trajectory, target_planet_trajectory)
-    dt = time[1]  # Nice to have the same time index as used by SpaceMission. Multiplying by 100 to get faster plot
+    home_planet_trajectory = planet_positions[:, 0, :]
+    home_planet_initial_velocity = system.initial_velocities[:, 0]
+    target_planet_trajectory = planet_positions[:, 1, :]
+    smallest_distance, time_index = find_closest_orbit(
+        home_planet_trajectory, target_planet_trajectory)
+    # Nice to have the same time index as used by SpaceMission. Multiplying by 100 to get faster plot
+    dt = time[1]
 
-    r_home_target =  target_planet_trajectory[:,time_index] - home_planet_trajectory[:,time_index] # vector from home planet to target planet we can use to direct the initial velocity vector
-    
+    # vector from home planet to target planet we can use to direct the initial velocity vector
+    r_home_target = target_planet_trajectory[:,
+                                             time_index] - home_planet_trajectory[:, time_index]
+
     t = time_index*dt
     T = time_index*dt + 5
-    angle_r_home_target = (np.arctan(r_home_target[1]/r_home_target[0])) + np.pi 
+
     # Finds the angle of the vector pointing from home to target. We use this to calculate the start position of our rocket launch
-    angle = np.pi * 5/4
-    
+
     planet_r = utils.km_to_AU(system.radii[0])
     '''
     To place the rocket at the closest position between the planets and on the surface of the home planet. we find the angle of the vector pointing between the planets. We use this angle and polar coordinates to place the rocket at the correct position.
     '''
-    
-    r0 = home_planet_trajectory[:,time_index] + np.array([0,-planet_r]) # Sets the rocket on the bottom of the planet
-    
-    
+
+    # Sets the rocket on the bottom of the planet
+    r0 = home_planet_trajectory[:, time_index] + np.array([0, -planet_r])
+
     final_time_index = int(T/dt)
-    plt.plot(home_planet_trajectory[0,time_index:final_time_index], home_planet_trajectory[1,time_index:final_time_index], label = 'Home planet trajectory')
-    plt.plot(target_planet_trajectory[0,time_index:final_time_index], target_planet_trajectory[1,time_index:final_time_index], label = 'Target planet trajectory')
-    
-    # angle = 261.75*np.pi/180
-    # v0 = np.array([np.cos(angle), np.sin(angle)]) * 4.75
-    # final_time, shuttle_velocity, shuttle_position = trajectory(t, T, dt, v0, r0)
-    # plt.plot(shuttle_position[:25000,0], shuttle_position[:25000,1])
-    # check_close_enough()
+    plt.plot(home_planet_trajectory[0, time_index:final_time_index],
+             home_planet_trajectory[1, time_index:final_time_index], label='Home planet trajectory')
+    plt.plot(target_planet_trajectory[0, time_index:final_time_index],
+             target_planet_trajectory[1, time_index:final_time_index], label='Target planet trajectory')
 
-
-    angles = np.linspace(260 * np.pi/180, 265 * np.pi/180, 5)
-    velocities = np.linspace(4.70, 4.80, 5)
+    angles = np.linspace(234 * np.pi/180, 236 * np.pi/180, 5)
+    velocities = np.linspace(7.25, 7.75, 3)
     finished = False
     for angle in angles:
+        print(f'{angle*180/np.pi: .2f}')
         for velocity in velocities:
+            print(f'\t{velocity}')
             v0 = np.array([np.cos(angle), np.sin(angle)]) * velocity
-            final_time, shuttle_velocity, shuttle_position = trajectory(t, T, dt, v0, r0)
+            final_time, shuttle_velocity, shuttle_position = trajectory(
+                t, T, dt, v0, r0)
             if check_close_enough():
-                print('success')
-                good_enough_velocity = velocity     
+                good_enough_velocity = velocity
                 good_enough_angle = angle
-                print(f'{good_enough_angle*180/np.pi} degrees were good enough')
-                print(f'{good_enough_velocity} was fast enough')
-                plt.plot(shuttle_position[:25000,0], shuttle_position[:25000,1], label = 'Shuttle trajectory')
+                print(f'{good_enough_angle*180/np.pi: .2f} degrees were good enough')
+                print(f'{good_enough_velocity: .2f} was fast enough')
                 finished = True
-                
+
+            plt.plot(
+                shuttle_position[:5000, 0], shuttle_position[:5000, 1])
+
             if finished:
                 break
         if finished:
             break
-                
 
-                
-
-
-
-
-    # min_angle = 261.7750520*np.pi/180
-    # max_angle = 261.7750524*np.pi/180 
-    
-    # angles = np.linspace(min_angle, max_angle, 10)
-    # for i in range(len(angles)):
-    #     v0 = np.array([np.cos(angles[i]), np.sin(angles[i])]) * 4.75 - 0.00000005
-    #     final_time, shuttle_velocity, shuttle_position = trajectory(t, T, dt, v0, r0)
-    #     plt.plot(shuttle_position[:25000,0], shuttle_position[:25000,1], color =  colors[i])
-    #     check_distance_from_target()
-        
-    # min_vel = 4.75 - 0.00005
-    # max_vel = 4.75 - 0.00006
-    # velocities = np.linspace(min_vel, max_vel, 10)
-    # angle = (max_angle + min_angle) / 2
-    # for v in velocities:
-    #     v0 = np.array([np.cos(angle), np.sin(angle)])  * v
-    #     final_time, shuttle_velocity, shuttle_position = trajectory(t, T, dt, v0, r0)
-    #     plt.plot(shuttle_position[:25000,0], shuttle_position[:25000,1])
-    #     check_close_enough()
-    # for i in range(time_index, final_time_index, 10):
-        
-    #     plt.scatter(home_planet_trajectory[0,i], home_planet_trajectory[1,i], label = f'Home planet at time index {i}', color = 'green')
-    #     plt.scatter(target_planet_trajectory[0,i], target_planet_trajectory[1,i], label = f'Target planet at time index {i}', color = 'green')
-    #     plt.scatter(shuttle_position[int(i - time_index),0], shuttle_position[int(i - time_index),1], label = f'Shuttle position at time index {i}', color = 'pink')
-        
-    
-    
-    
     plt.xlabel('x [AU]')
     plt.ylabel('y [AU]')
-    plt.legend(loc = 'upper right')
+    plt.legend(loc='upper right')
+    plt.axis('equal')
     plt.show()
-    
-    
-    
+
     '''
     Plotting the trajectory of home and target planet and where they are the closest
     '''
