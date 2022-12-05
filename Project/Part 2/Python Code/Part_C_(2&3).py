ebefeb8eb621d1e72b813c2d593a4800dc188676
Oskar-Idland <oskaridland@hotmail.com>
2022-12-05 21:56:22 +0100

    Finished part 2

--- Project/Part 2/Python Code/Part_C_(2&3).py
+++ Project/Part 2/Python Code/Part_C_(2&3).py
@@ -1,12 +1,15 @@
-from Part_C import integrate
+from Part_C import calc_orbit
 from Initialize_System import *
 from scipy import interpolate
 import matplotlib.pyplot as plt
 import numpy as np
-
+import os
 
 #CONSTANTS
-G = 4*(np.pi**2)  #Gravitational Constant
+G = 4*(np.pi**2)    #Gravitational Constant
+Planet = 2          # Our planet of choice for simulation
+star_mass = system.star_mass
+planet_mass = system.masses[Planet]
 
 #CALCULATED DATA
 T = 5*20  #You want to find the planetary position from t=0 to t=T. How could you make an educated guess for T?
@@ -18,8 +21,7 @@ T = 5*20  #You want to find the planetary position from t=0 to t=T. How could yo
 N = 30000  #Number of time steps
 dt = T/N  #calculate time step from T and N
 
-
-Planet = 2
+# Initial conditions for planet and star
 P_x0 = system.initial_positions[0,Planet]
 P_y0 = system.initial_positions[1,Planet]
 P_vx0 = system.initial_velocities[0,Planet] 
@@ -36,23 +38,28 @@ S_x0 = -com[0]
 S_y0 = -com[1]
 
 def radial_velocity():
-    t, r_Planet, v_Planet, r_Star, v_Star  = integrate(T, dt, N, S_x0, S_y0, S_vx0, S_vy0, P_x0, P_y0, P_vx0, P_vy0, G, star_mass, planet_mass)
+    '''
+    Function which creates radial velocity curve with noise added on top\n
+    Writes this to file named 'Radial Velocity.txt'
+    '''
+    t, r_Planet, v_Planet, r_Star, v_Star  = calc_orbit(T, dt, N, S_x0, S_y0, S_vx0, S_vy0, P_x0, P_y0, P_vx0, P_vy0, G, star_mass, planet_mass)
 
     angle = np.pi/4 # Arbitrary angle
     v_pec = .0000035 # Arbitrary velocity
-    v_Star = np.array([np.linalg.norm(v) for v in v_Star]) #Changing the shape of the array from (3000,2) -> (3000,)
+    v_Star = np.array([np.linalg.norm(v) for v in v_Star]) #Calculating magnitude of velocity and changing the shape of the array from (3000,2) -> (3000,) 
+    
     v_rad = v_Star * np.sin(angle) - v_pec 
     m = np.max(v_rad)
     noise = np.random.normal(0, 1/5*m, N) #Gaussian probability distribution with mean = 0 stddev = 1/5 of the max v_rad
     plt.plot(t, v_rad + noise, c = 'black')
     plt.title('Radial Velocity')
     plt.show() 
-    # plt.savefig(os.path.join("Figures\Radial Velocity"), format = 'pdf')
-    # with open("Radial Velocity", 'w') as file: 
-    #     file.write('time [Yr]               Velocity [AU]\n')
-    #     for i in range(len(t)):
-    #         file.write(f'{t[i]: <22} {v_rad[i]+noise[i]: >22}\n') 
-    # TODO Undo commenting before delivery
+    plt.savefig(os.path.join("Figures\Radial Velocity"), format = 'pdf')
+    
+    with open("Radial Velocity", 'w') as file: 
+        file.write('time [Yr]               Velocity [AU]\n')
+        for i in range(len(t)):
+            file.write(f'{t[i]: <22} {v_rad[i]+noise[i]: >22}\n') 
     
 def read_radial_velocity(filename):
     '''
@@ -71,19 +78,12 @@ def read_radial_velocity(filename):
             v_rad[i], t[i] = float(velocity), float(time)
     return t, v_rad
 
-# t, v_rad = read_radial_velocity('Radial Velocity')
-# plt.plot(t,v_rad)
-# plt.show()
-
-# TODO: Remove comment before delivery 
-# radial_velocity() 
-
-
-
 
 def radial_velocity_analysis(t,v_rad):
+    '''
+    Function to clean noisy data by averaging  out a subset of data to calculate an estimated value for each data point in that subset
+    '''
     k = 200
-
     plt.plot(t[k:-k], v_rad[k:-k], label = 'Velocity with noise')
 
 
@@ -96,13 +96,19 @@ def radial_velocity_analysis(t,v_rad):
     v_filtered *= 1/(k)
     plt.plot(t, v_filtered, label = 'Cleansed data')
     plt.legend()
-    # plt.savefig("Figures\\Other_Group_Radial_Velocity")
+    plt.savefig("Figures\\Other_Group_Radial_Velocity")
     plt.show()
     
-t, v_rad = read_radial_velocity('velocitydata.txt')
-radial_velocity_analysis(t,v_rad)
-v_rad, t = read_radial_velocity('velocitydata3.txt')
-radial_velocity_analysis(t,v_rad)
 
+if __name__ == "__main__":
+    t, v_rad = read_radial_velocity('Radial Velocity')
+    plt.plot(t,v_rad)
+    plt.show()
+    
+    radial_velocity() 
 
-
+    # Calculated velocity data from other groups
+    t, v_rad = read_radial_velocity('velocitydata.txt')
+    radial_velocity_analysis(t,v_rad)
+    v_rad, t = read_radial_velocity('velocitydata3.txt')
+    radial_velocity_analysis(t,v_rad)
